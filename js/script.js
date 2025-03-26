e66b8e2b7a0dcf3cb92963bda6074c9435b9ccb6
Piotr Rulewicz <piotr.rulewicz@hotmail.com>
2025-03-26 11:26:27 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+{
+
+    const init = () => {
+        const form = document.getElementById("form");
+        const inputDimension = document.getElementById("inputDimension");
+        const selectedScale = document.getElementById("selectedScale");
+        const resultAfterRescaling = document.getElementById("resultAfterRescaling");
+        const resetButton = document.getElementById("resetButton");
+
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            const userInputDimension = inputDimension.value;
+            const userSelectedScale = selectedScale.value;
+
+            console.log(userInputDimension);
+            console.log(userSelectedScale);
+
+            let rescaling;
+
+            if (userSelectedScale === "h0") {
+                rescaling = userInputDimension * 1000 / 87;
+            } else if (userSelectedScale === "tt") {
+                rescaling = userInputDimension * 1000 / 120;
+            } else if (userSelectedScale === "nn") {
+                rescaling = userInputDimension * 1000 / 160;
+            }
+
+            resultAfterRescaling.textContent = rescaling.toFixed(0);
+
+        });
+
+        resetButton.addEventListener("click", function () {
+            resultAfterRescaling.textContent = "0";
+        });
+
+    };
+
+    init();
+
+};
